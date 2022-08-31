45c8bf86c7909fdce08a08c697eb1e8f6ecae113
MatiasMarazza <matias.marazza@smartway.com.ar>
2022-08-31 12:23:07 -0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,63 @@
-import 'grapesjs/dist/css/grapes.min.css';
-import grapesjs from 'grapesjs';
-import 'grapesjs-preset-webpage';
-import { GrapesjsReact } from 'grapesjs-react';
+import { useEffect } from "react";
+import grapesjs from 'grapesjs'
+import 'grapesjs/dist/css/grapes.min.css'
+import 'grapesjs/dist/grapes.min.js'
+import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.css'
+import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.js'
 
 function App() {
-  
+ useEffect(() => {
+   grapesjs.init({
+     container: '#gjs',
+     height: '1080px',
+     width: '100%',
+     plugins: ['gjs-preset-webpage'],
+     storageManager: {
+       id: 'gjs-',
+       type: 'local',
+       autosave: true,
+       storeComponents: true,
+       storeStyles: true,
+       storeHtml: true,
+       storeCss: true,
+     },
+     deviceManager: {
+       devices:
+       [
+         {
+           id: 'desktop',
+           name: 'Desktop',
+           width: '',
+           height:'1080px'
+         },
+         {
+           id: 'tablet',
+           name: 'Tablet',
+           width: '768px',
+           widthMedia: '992px',
+         },
+         {
+           id: 'mobilePortrait',
+           name: 'Mobile portrait',
+           width: '320px',
+           widthMedia: '575px',
+         },
+       ]
+     },
+     pluginsOpts: {
+       'grapesjs-preset-webpage': {
+         blocksBasicOpts: {
+           blocks: ['column1', 'column2', 'column3', 'column3-7', 'text',     'link', 'image', 'video'],
+           flexGrid: 1,
+         },
+         blocks: ['link-block', 'quote', 'text-basic'],
+       },
+     }
+   })
+ },[])
 
-   
-  return (
-   <div className='wrapper'>
-   <GrapesjsReact
-    id='grapesjs-react'
-    plugins={[
-      'gjs-preset-webpage',
-      'gjs-blocks-basic'
-    ]}
-  />
-   </div>
-  )
+ return (
+   <div id="gjs"></div>
+ );
 }
-
-export default App
+export default App;
