45ae4d6d4125e1f81864a419877cb46cef4cedde
MatiasMarazza <matias.marazza@smartway.com.ar>
2022-09-09 11:21:24 -0300

    addec custom video element

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,68 @@ function App() {
      
     //  scripts: ['https://.../somelib.min.js'], acá hay que agregar el script del streaming
    })
+  //  editor.DomComponents.addType('video-hls', {
+  //   // Make the editor understand when to bind `my-input-type`
+  //   isComponent: el => el.tagName === 'INPUT',
+  
+  //   // Model definition
+  //   model: {
+  //     // Default properties
+  //     defaults: {
+  //       tagName: 'video',
+  //       draggable: true, 
+  //       droppable: true, // Can drop other elements inside
+  //       attributes: { // Default attributes
+  //         type: 'video',
+  //         name: 'video-hls',
+  //         placeholder: 'm3u8',
+  //         allowfullscreen: 'allowfullscreen'
+  //       },
+  //       traits: [
+  //         'name',
+  //         'placeholder',
+  //         { type: 'checkbox', name: 'required' },
+  //       ],
+  //     }
+  //   }
+  // });
+
+  // comps.addType('video-hls', {
+  //   isComponent: el => {el.tagName == 'video'},
+  //   extend: 'other-defined-component',
+  //   model: { 
+  //     defaults:{
+
+  //     }
+  //    }, // Will extend the model from 'other-defined-component'
+  //   view: { ... }, // Will extend the view from 'other-defined-component'
+  // });
+
+  const { DomComponents, Blocks } = editor;
+
+  DomComponents.addType("custom-video", {
+    extend: "video",
+    view: {
+      events: {
+        dblclick: "handleDblClick"
+      },
+      handleDblClick() {
+        alert("HOla mundo!");
+      }
+    }
+  });
+  
+  
+  // Add a block
+  Blocks.add("Video", {
+    label: "Video HLS",
+    attributes: { class: "fa fa-youtube-play HLS" },
+    content: {
+      type: "custom-video"
+    }
+  });
+  
+
    editor.Components.addType('wrapper', {
     model: {
       defaults: {
