d381595724a9e21c7f847c990b51f10a3fe153c2
MatiasMarazza <matias.marazza@smartway.com.ar>
2022-09-19 08:21:33 -0300

    commented anim

--- plugins/wrapperPlugin.js
+++ plugins/wrapperPlugin.js
@@ -5,12 +5,28 @@ export default function wrapperPlugin(editor){
    editor.Components.addType('wrapper', {
     model: {
       defaults: {
-        tagName: 'section'
+        tagName: 'section',
+        stylable: [
+          // Default attributes
+          'background',
+          'background-color',
+          'background-image',
+          'background-repeat',
+          'background-attachment',
+          'background-position',
+          'background-size',
+
+          // Add the "Dimension" sector attributes
+          'width',
+          'height', 
+
+      ]
       },
     
     }})
 }
 var editor = grapesjs.init({
     container:'#gjs',
-    plugins:[wrapperPlugin]
+    plugins:[wrapperPlugin],
+    
 })
--- src/App.jsx
+++ src/App.jsx
@@ -51,84 +51,65 @@ function App() {
      pluginsOpts: {
        'grapesjs-preset-webpage': {
          blocksBasicOpts: {
-           blocks: ['column1', 'column2', 'column3', 'column3-7', 'text',     'link', 'image', 'video'],
+           blocks: ['column1', 'column2', 'column3', 'column3-7', 'text', 'link', 'image', 'video'],
            flexGrid: 1,
          },
          blocks: ['link-block', 'quote', 'text-basic'],
        },
      },
-     	exportWrapper: false,
-       wrapperIsBody: false,
+     	exportWrapper: true,
+       wrapperIsBody: true,
        dragMode: 'absolute',
      
      
       scripts: ['https://cdn.jsdelivr.net/npm/hls.js@1']
    })
 
-  //  const stream = function(props) {
-  //   const initLib = function() {
-  //     const el = this;
-  //     const myLibOpts = {
-  //       prop1: props.myprop1,
-  //       prop2: props.myprop2,
-  //     };
-  //     someExtLib(el, myLibOpts);
-  //   };
-  
-  //   if (typeof someExtLib == 'undefined') {
-  //     const stream = document.createElement('stream');
-  //     stream.onload = initLib;
-  //     stream.src = 'https://cdn.jsdelivr.net/npm/hls.js@1';
-  //     document.body.appendChild(stream);
-  //   } else {
-  //     initLib();
-  //   }
-  // };
 
-  const def = editor.Components.getType("default");
+// const def = editor.Components.getType("default");
 
-editor.Components.addType("default", {
-   model:{
-      defaults:{
-         duration: 2,
-         delay: 0,
-         traits:[
-            ...def.model.prototype.defaults.traits,
-            ...[{
-                   changeProp: 1,
-                   type: "select",
-                   label: "Animation",
-                   name: "animation",
-                   options:[
-                     {value: 'bounce',name: 'Bounce'},
-                     //Other animations...
-                   ]
-             }, {
-                   changeProp: 1,
-                   type: "number",
-                   label: "Duration(s)",
-                   name: "duration",
-             }, {
-                   changeProp: 1,
-                   type: "number",
-                   label: "Delay(s)",
-                   name: "delay",
-             }]
-          ]
-       },
-       init() {
-          this.on("change:animation", this.onAnimationChange);
-          this.on("change:duration", this.onAnimationChange);
-          this.on("change:delay", this.onAnimationChange);
-       },
-       onAnimationChange() {
-          const animation = this.get("animation");
-          const duration = this.get("duration");
-          const delay = this.get("delay");
-          this.addStyle({ "animation": `${animation} ${duration}s ${delay}s infinite` });
-       }
-    }
-});
+// editor.Components.addType("default", {
+//    model:{
+//       defaults:{
+//          duration: 2,
+//          delay: 0,
+//          traits:[
+//             ...def.model.prototype.defaults.traits,
+//             ...[{
+//                    changeProp: 1,
+//                    type: "select",
+//                    label: "Animation",
+//                    name: "animation",
+//                    options:[
+//                      {value: 'bounce',name: 'Bounce'},
+//                      //Other animations...
+//                    ]
+//              }, {
+//                    changeProp: 1,
+//                    type: "number",
+//                    label: "Duration(s)",
+//                    name: "duration",
+//              }, {
+//                    changeProp: 1,
+//                    type: "number",
+//                    label: "Delay(s)",
+//                    name: "delay",
+//              }]
+//           ]
+//        },
+//        init() {
+//           this.on("change:animation", this.onAnimationChange);
+//           this.on("change:duration", this.onAnimationChange);
+//           this.on("change:delay", this.onAnimationChange);
+//        },
+//        onAnimationChange() {
+//           const animation = this.get("animation");
+//           const duration = this.get("duration");
+//           const delay = this.get("delay");
+//           this.addStyle({ "animation": `${animation} ${duration}s ${delay}s infinite` });
+//        }
+//     }
+// });
 
 
  },[])
